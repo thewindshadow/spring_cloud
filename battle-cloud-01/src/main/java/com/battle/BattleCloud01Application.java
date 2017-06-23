3711c82913e5ac375bea3c19e961911f91eb62e5
zhangbing <zhangbingbeyondbit@163.com>
2017-06-23 11:12:35 +0800

    Initial commit

---
+++ battle-cloud-01/src/main/java/com/battle/BattleCloud01Application.java
@@ -0,0 +1,12 @@
+package com.battle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleCloud01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleCloud01Application.class, args);
+	}
+}
